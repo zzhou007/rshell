c8680c712afba4a8abd61ddaca008e29b244880b
zzhou007 <zzhou007@ucr.edu>
2015-04-11 17:37:36 -0700

    added rshell code works without connector

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -1,8 +1,43 @@
+#include <string.h>
 #include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
-	cout << "hello" << endl;
+	string in;
+	int waits;
+	for(;;)
+	{
+		cout << "$";
+		getline(cin,in);
+		pid_t pid = fork();
+		if (pid == -1)
+			perror("failed to fork");
+		else if (pid == 0)
+		{
+			char *arg[in.length()];
+			char *temp = strdup(in.c_str());
+			arg[0] = strtok(temp, " ");
+			char *token = arg[0];
+			for (int i = 1; token != NULL; i++)
+			{
+				arg[i] = strtok(NULL, " ");
+				token = arg[i];
+			}
+			int execs = execvp(arg[0], arg);
+			if (execs == -1)
+				perror("cannot execute command");
+		}
+		else 
+		{
+			
+			waits = wait(NULL);
+			if (waits == -1)
+				perror("no child");
+		}
+	}
 	return 0;
 }
